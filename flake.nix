cdf9e39f7359508457f2bb1e3587c6114fe97b59
Jake Crowell <jasperdaniels91@icloud.com>
2022-08-28 03:53:58 -0400

    N/A

--- flake.nix
+++ flake.nix
@@ -1,24 +1,23 @@
 {
-  description = "My Home Manager Flake";
-
   inputs = {
-    # Specify the source of Home Manager and Nixpkgs.
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "nixpkgs/nixos-unstable";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
-  
-  outputs = { nixpkgs, home-manager, ... }:
-  {
-    defaultPackage.x86_64-linux = home-manager.defaultPackage.x86_64-linux;
 
-    homeConfigurations = {
-      "jake" = home-manager.lib.homeManagerConfiguration {
-        pkgs = nixpkgs;
-        modules = [ ./home.nix ];
-      };
+  outputs = { nixpkgs, home-manager, ... }@inputs:
+  let 
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in {
+    homeConfigurations.jake = home-manager.lib.homeManagerConfiguration {
+      inherit pkgs;
+
+      modules = [
+        ./home.nix
+      ];
     };
   };
 }
